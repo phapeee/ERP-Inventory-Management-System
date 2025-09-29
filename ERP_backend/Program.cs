cd5b1fe1326d55eeb4f313ed36da379189c48ca8
Khuong Nguyen <phapeeee@gmail.com>
2025-09-29 14:20:41 -0400

    Setup API controller and PostgreSQL connection

--- ERP_backend/Program.cs
+++ ERP_backend/Program.cs
@@ -1,8 +1,22 @@
+using ERP_backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Launch the app and navigate to https://localhost:<port>/openapi/v1.json to view the generated OpenAPI document.
 builder.Services.AddOpenApi();
+
+// Setup link: https://www.c-sharpcorner.com/article/building-a-powerful-asp-net-core-web-api-with-postgresql/
+// Offical setup link: https://www.npgsql.org/efcore/index.html?tabs=onconfiguring
+// Use .NET (C#) to connect and query data in Azure Database for PostgreSQL flexible server: https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/connect-csharp?utm_source=chatgpt.com
+builder.Services.AddDbContext<AppDb>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
@@ -10,29 +24,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();  // UI at /scalar
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.MapControllers();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
---
+++ useful_commands.md
@@ -0,0 +1,26 @@
+## PostgreSQL docker
+
+docker run --name pg \
+  -e POSTGRES_USER=app \
+  -e POSTGRES_PASSWORD=secret \
+  -e POSTGRES_DB=erp \
+  -p 5432:5432 \
+  -v pgdata:/var/lib/postgresql/data \
+  -d postgres:16
+
+## API controller with CRUD actions using EF Core (not working on .NET 9)
+
+dotnet aspnet-codegenerator controller \
+  -name ProductsController \
+  -api \
+  -m Product \
+  -dc AppDb \
+  -outDir Controllers
+
+## Create simple API controller
+
+dotnet new apicontroller \
+  -n <controller_name> \
+  -o Controllers \
+  --namespace ERP_backend.Controllers
+  
